6c9d382c0e12e69164ed4f9b34d80c1e0d3b7b3c
Jeyanandh V S <155248243+Jack-Sparrow-7@users.noreply.github.com>
2025-09-28 23:16:57 +0530

    counter

---
+++ counter/counter.v
@@ -0,0 +1,15 @@
+module counter(clk,reset,enable,count);
+
+input clk,reset,enable;
+output reg [0:7] count;
+
+always @(posedge clk or posedge reset) begin
+    if (reset) begin
+        count <= 8'b0;
+    end
+    else if (enable) begin
+        count <= count + 1;
+    end
+end
+
+endmodule
---
+++ counter/counter_tb.v
@@ -0,0 +1,34 @@
+module counter_tb;
+
+    reg clk,enable,reset;
+    wire [0:7] count;
+
+    counter uut(.clk(clk),.enable(enable),.reset(reset),.count(count));
+
+    initial clk = 0;
+    always #5 clk = ~clk;
+
+    initial begin
+        $dumpfile("counter_wave.vcd");
+        $dumpvars(0,counter_tb);
+
+        reset = 1; enable = 0;
+        
+        #10 reset = 0;
+        #10 enable = 1;
+
+        #100;
+
+        enable = 0;
+        #20;
+
+        reset = 1;
+        #10 reset = 0;
+
+        enable = 1;
+        #50;
+
+        $finish;
+    end
+
+endmodule
