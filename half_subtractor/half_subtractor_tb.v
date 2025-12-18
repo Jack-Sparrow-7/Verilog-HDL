ee0c025b9b736a6f5c4d744a9bb52298dffe5bc2
Jeyanandh V S <155248243+Jack-Sparrow-7@users.noreply.github.com>
2025-12-18 12:21:33 +0530

    finished half subtractor

---
+++ half_subtractor/half_subtractor_tb.v
@@ -0,0 +1,19 @@
+module half_subtractor_tb;
+    reg a,b;
+    wire diff,borrow;
+
+    half_subtractor dut(.a(a),.b(b),.diff(diff),.borrow(borrow));
+
+    initial begin
+        $dumpfile("half_subtractor_wave.vcd");
+        $dumpvars(0,half_subtractor_tb);
+
+        $monitor("a = %b | b = %b | difference = %b | borrow = %b",a,b,diff,borrow);
+
+        a = 0; b = 0; #10;
+        a = 0; b = 1; #10;
+        a = 1; b = 0; #10;
+        a = 1; b = 1; #10;
+        $finish;
+    end
+endmodule
