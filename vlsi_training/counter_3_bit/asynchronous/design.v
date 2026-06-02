0d64d9b9d5645eee55bf07148a0ba072471b529d
Jeyanandh V S <155248243+Jack-Sparrow-7@users.noreply.github.com>
2026-06-02 08:28:28 +0530

    Add asynchronous and synchronous 3-bit counter implementations with testbenches and simulation files

---
+++ vlsi_training/counter_3_bit/asynchronous/design.v
@@ -0,0 +1,18 @@
+module counter_3_bit (
+    clk,
+    reset,
+    count
+);
+    input clk, reset;
+    output reg [2:0] count;
+
+    initial begin
+        count = 3'b000;
+    end
+
+    always @(posedge clk or posedge reset) begin
+        if (reset) count = 3'b000;
+        else count += 1;
+    end
+
+endmodule
---
+++ vlsi_training/counter_3_bit/asynchronous/testbench.v
@@ -0,0 +1,36 @@
+module testbench;
+    reg clk, reset;
+    wire [2:0] count;
+
+    counter_3_bit uut (
+        clk,
+        reset,
+        count
+    );
+
+    always #5 clk = ~clk;
+
+    initial begin
+        $monitor("clk = %b | reset = %b | count = %b or %d", clk, reset, count,
+                 count);
+        clk   = 0;
+
+
+        reset = 1;
+        #10;
+
+        reset = 0;
+
+        #180;
+
+        reset = 1;
+        #10;
+
+        reset = 0;
+
+        #20;
+
+        $finish;
+    end
+
+endmodule
